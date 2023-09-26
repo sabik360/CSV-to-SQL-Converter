c7495155250a4b61bde4308b8476b1fa44cb9f50
Sabik <92353902+sabik360@users.noreply.github.com>
2023-09-26 18:40:25 +0530

    adding main files

---
+++ requirements.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: Install Chocolatey package manager
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Install Python 3
+choco install python
+
+:: Install PyQt5
+pip install pyqt5
